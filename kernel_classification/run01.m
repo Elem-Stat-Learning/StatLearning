2beec3162a42d3ce51f8bd20558135612fea0040
vadik <vadik@vadServer.wadzapi>
2013-05-19 15:28:50 +0400

    different

--- kernel_classification/run01.m
+++
@@ -1,9 +0,0 @@
-clear all;
-close all;
-clc;
-%load data form SAheart dataset file
-data = dlmread('../datasets/SAheart.data', ',', 1, 1);
-X = data(:, 1:9);
-y = data(:, 10);
-[train_idx, test_idx] = stratifiedSampling(y, 0.3);
-%train and test on non-normalized features
